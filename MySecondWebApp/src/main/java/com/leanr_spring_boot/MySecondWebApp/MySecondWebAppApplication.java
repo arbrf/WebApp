469317604fa6cedb5c986d3d95870dd305b21387
Harry <shaikmohammadibrahim311@gmail.com>
2024-06-10 22:06:56 +0530

    Completed upto view resolver

---
+++ MySecondWebApp/src/main/java/com/leanr_spring_boot/MySecondWebApp/MySecondWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.leanr_spring_boot.MySecondWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySecondWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySecondWebAppApplication.class, args);
+	}
+
+}
---
+++ MySecondWebApp/src/test/java/com/leanr_spring_boot/MySecondWebApp/MySecondWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leanr_spring_boot.MySecondWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySecondWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
