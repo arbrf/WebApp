b848ac45f3b38543041fb468759c421579afa0c6
Harry <shaikmohammadibrahim311@gmail.com>
2024-06-23 11:26:38 +0530

    no message

---
+++ restful-web-servicrs/src/main/java/com/IN28MINUTES/rest/webservices/restful_web_servicrs/RestfulWebServicrsApplication.java
@@ -0,0 +1,13 @@
+package com.IN28MINUTES.rest.webservices.restful_web_servicrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicrsApplication.class, args);
+	}
+
+}
---
+++ restful-web-servicrs/src/test/java/com/IN28MINUTES/rest/webservices/restful_web_servicrs/RestfulWebServicrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IN28MINUTES.rest.webservices.restful_web_servicrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
