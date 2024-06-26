aad7d201f7b61878d51899329a18bc74ffae85d9
Harry <shaikmohammadibrahim311@gmail.com>
2024-06-26 22:03:08 +0530

    ResponseStatus

---
+++ restful-web-servicrs/src/main/java/com/IN28MINUTES/rest/webservices/restful_web_servicrs/controller/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package com.IN28MINUTES.rest.webservices.restful_web_servicrs.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+	
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+
+}
--- restful-web-servicrs/src/main/java/com/IN28MINUTES/rest/webservices/restful_web_servicrs/controller/UserResource.java
+++ restful-web-servicrs/src/main/java/com/IN28MINUTES/rest/webservices/restful_web_servicrs/controller/UserResource.java
@@ -1,31 +1,55 @@
 package com.IN28MINUTES.rest.webservices.restful_web_servicrs.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.IN28MINUTES.rest.webservices.restful_web_servicrs.user.User;
 import com.IN28MINUTES.rest.webservices.restful_web_servicrs.user.UserDaoService;
 
 @RestController
 public class UserResource {
+
 	private UserDaoService service;
 
 	public UserResource(UserDaoService service) {
-		super();
 		this.service = service;
 	}
+
 	@GetMapping("/users")
-   private List<User> retrieveAllUsers(){
-	   
-	   return service.findAll();
-   }
-	
+	public List<User> retrieveAllUsers() {
+		return service.findAll();
+	}
+
 	@GetMapping("/users/{id}")
-	   private User retrieveAllUsers(@PathVariable Integer id){
-		   
-		   return service.findById(id);
-	   }
+	public User retrieveUser(@PathVariable int id) {
+		User user = service.findOne(id);
+		
+		if(user==null)
+			throw new UserNotFoundException("id:"+id);
+		
+		return user;
+	}
+
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		
+		User savedUser = service.save(user);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+						.path("/{id}")
+						.buildAndExpand(savedUser.getId())
+						.toUri();   
+		
+		return ResponseEntity.created(location).build();
+	}
+	
+	
 }
